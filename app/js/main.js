4d20ecb6d3671c9941fe7a7e827f80021ac798f3
Osycle <osycle@gmail.com>
2018-08-16 22:52:06 +0500

    16.08

--- app/js/main.js
+++ app/js/main.js
@@ -38,47 +38,47 @@
 			html: true
 		});
 		
-		/* datepicker */
-		( function( factory ) {
-			if ( typeof define === "function" && define.amd ) {
-				// AMD. Register as an anonymous module.
-				define( [ "../widgets/datepicker" ], factory );
-			} else {
-				// Browser globals
-				factory( jQuery.datepicker );
-			}
-		}( function( datepicker ) {
+		// /* datepicker */
+		// ( function( factory ) {
+		// 	if ( typeof define === "function" && define.amd ) {
+		// 		// AMD. Register as an anonymous module.
+		// 		define( [ "../widgets/datepicker" ], factory );
+		// 	} else {
+		// 		// Browser globals
+		// 		factory( jQuery.datepicker );
+		// 	}
+		// }( function( datepicker ) {
 
-		datepicker.regional.ru = {
-			closeText: "Закрыть",
-			prevText: "&#x3C;Пред",
-			nextText: "След&#x3E;",
-			currentText: "Сегодня",
-			monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
-			"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
-			monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
-			"Июл","Авг","Сен","Окт","Ноя","Дек" ],
-			dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
-			dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
-			dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
-			weekHeader: "Нед",
-			dateFormat: "dd.mm.yy",
-			firstDay: 1,
-			isRTL: false,
-			showMonthAfterYear: false,
-			yearSuffix: "" };
-		datepicker.setDefaults( datepicker.regional.ru );
+		// datepicker.regional.ru = {
+		// 	closeText: "Закрыть",
+		// 	prevText: "&#x3C;Пред",
+		// 	nextText: "След&#x3E;",
+		// 	currentText: "Сегодня",
+		// 	monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+		// 	"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+		// 	monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+		// 	"Июл","Авг","Сен","Окт","Ноя","Дек" ],
+		// 	dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+		// 	dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+		// 	dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+		// 	weekHeader: "Нед",
+		// 	dateFormat: "dd.mm.yy",
+		// 	firstDay: 1,
+		// 	isRTL: false,
+		// 	showMonthAfterYear: false,
+		// 	yearSuffix: "" };
+		// datepicker.setDefaults( datepicker.regional.ru );
 
-		return datepicker.regional.ru;
+		// return datepicker.regional.ru;
 
-		} ) );
-		$.datepicker.regional[ "ru" ];
+		// } ) );
+		// $.datepicker.regional[ "ru" ];
 
-    /*rating appraise*/
-    $(document).on("click", ".rating-appraise li", function(){
-      $(this).siblings().removeClass("cheсked")
-      $(this).addClass("cheсked");
-		});
+  //   /*rating appraise*/
+  //   $(document).on("click", ".rating-appraise li", function(){
+  //     $(this).siblings().removeClass("cheсked")
+  //     $(this).addClass("cheсked");
+		// });
 		$( "#datepicker" ).datepicker({
 			showOtherMonths: true,
       selectOtherMonths: true,
@@ -330,7 +330,9 @@
 			checkedToggle();
 		})
 
-
+		$("[data-hidephone]").on("click", function(e){
+			$($(this).attr("data-hidephone")).show()
+		})
 
 
 
